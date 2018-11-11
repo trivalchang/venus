ca6e8a8d463436f8bc6ec7209ba95ed86c54875e
developer <developer@Andyde-MacBook-Pro.local>
2018-11-11 21:40:45 +0800

    Subject: refine argument and loop template file to match Test Case:
    $ python matchTest.py -i OSD/MainMenu.JPG -t OSD/title_pic_foc.png OSD/
    title_pic_audio_foc.png OSD/title_pic_app_foc.png OSD/
    title_pic_setup_foc.png OSD/title_pic_sx_nor.png OSD/title_pic_nor.png

--- imagedisplaylabel.h
+++ imagedisplaylabel.h
@@ -1,4 +1,13 @@
 #ifndef IMAGEDISPLAYLABEL_H
 #define IMAGEDISPLAYLABEL_H
 
+#include <QLabel>
+class ImageDisplayLabel:public QLabel
+{
+public:
+    ImageDisplayLabel(QWidget *parent = NULL, Qt::WindowFlags f=0)
+        :QLabel(parent,f)
+        {}
+};
+
 #endif // IMAGEDISPLAYLABEL_H
