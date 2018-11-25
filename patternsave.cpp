9e093ada38c1c4b4a33d6ef93d684ca45779e707
developer <developer@Andyde-MacBook-Pro.local>
2018-11-25 21:38:44 +0800

    Subject: refine argument and loop template file to match Test Case:
    $ python matchTest.py -i OSD/MainMenu.JPG -t OSD/title_pic_foc.png OSD/
    title_pic_audio_foc.png OSD/title_pic_app_foc.png OSD/
    title_pic_setup_foc.png OSD/title_pic_sx_nor.png OSD/title_pic_nor.png

--- patternsave.cpp
+++ patternsave.cpp
@@ -1,5 +1,9 @@
 
+#include <QJsonDocument>
 #include <QJsonObject>
+#include <QJsonArray>
+#include <QFile>
+#include <QDebug>
 #include "patternsave.h"
 
 void patternSave::addItem(QString name, patternSave::PATTERN_TYPE type)
@@ -14,11 +18,27 @@ void patternSave::addItem(QString name, patternSave::PATTERN_TYPE type)
 void patternSave::save()
 {
     QJsonObject json;
+
+    QJsonArray jsonItemArray;
     patternSave::ITEM item;
+
+    json.insert("Hello", "I'm here");
 
     while (m_items.isEmpty() == false)
     {
+        QJsonObject jsonItem;
         item = m_items.first();
+        jsonItem.insert("Name", item.m_name);
+        jsonItem.insert("Type", item.m_type);
+        jsonItemArray.push_back(jsonItem);
         m_items.removeFirst();
     }
+
+    json.insert("Pattern", jsonItemArray);
+    QJsonDocument jsonDoc(json);
+
+    QFile jsonFile("Pattern.json");
+    jsonFile.open(QFile::WriteOnly);
+    jsonFile.write(jsonDoc.toJson());
+    jsonFile.close();
 }
